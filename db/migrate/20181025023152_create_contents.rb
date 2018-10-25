5d3ed943456d65d2ff67c0d587f1810fa95e4111
dantho <dantho972@gmail.com>
2018-10-25 05:13:18 +0200

    fin

---
+++ db/migrate/20181025023152_create_contents.rb
@@ -0,0 +1,11 @@
+class CreateContents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contents do |t|
+      t.text :body
+      t.references :pins, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
